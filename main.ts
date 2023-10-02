fa6b52d5dc15ef79397f824ac26d1b1a222d9b3e
marrodri <laza.mar64@gmail.com>
2023-10-01 21:58:23 -0700

    access to argvs from CLI implemented

--- main.ts
+++ main.ts
@@ -1,8 +1,24 @@
-import * as readline from 'readline';
-function main(){
-    console.log("hello world");
-    var input = process.argv[1];
+import * as readline from "readline";
+function main() {
+  console.log("hello world");
+  var day = process.argv[2];
+  var month = process.argv[3];
+  var year = process.argv[4];
+  if (day == undefined) {
+    console.log("day not defined");
+  } else {
+    console.log(`day:${day}`);
+  }
+  if (month == undefined) {
+    console.log("month not defined");
+  } else {
+    console.log(`month:${month}`);
+  }
+  if (year == undefined) {
+    console.log("year not defined");
+  } else {
+    console.log(`year:${year}`);
+  }
 }
-
 
 main();
