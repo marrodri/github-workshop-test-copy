748e739e8698bd1edd31855e5b64ee1a0f0f0b7e
marrodri <laza.mar64@gmail.com>
2023-10-01 22:05:39 -0700

    dateToWeekDay function added with todos

--- functions/DateToWeekDay.ts
+++ functions/DateToWeekDay.ts
@@ -1,3 +1,12 @@
-
-
 // Todo create a dateToWeekDay function.
+
+interface dateToWeekInterface {
+  day: number;
+  month: number;
+  year: number;
+}
+
+function dateToWeekday({ day, month, year }: dateToWeekInterface) {
+  //TODO build a date toWeekday function that prints out
+  
+}
